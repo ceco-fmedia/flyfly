9fd93fc1bbda4948b64ce526b227805dfceca52b
ceco-fmedia <ceco.fmedia@gmail.com>
2020-02-02 12:43:09 +0200

    Removed timers.

--- oxygen_scrubber.gd
+++ oxygen_scrubber.gd
@@ -1,23 +1,34 @@
 extends Area2D
 
 var current_level = 1
+var timeLeft = 0
 
 func _ready():
 	pass
 
 func change_level(level):
-	if level != 2 or level<current_level:
+	if level == 2:
+		lvl2Animation()
+	elif level<current_level:
 		$Anim.play("level_"+str(level))
 	current_level = level
 
 func canUse(player):
 	return overlaps_body(player)
-func _on_Timer_timeout():
+
+func lvl2Animation():
 	print("wwwwww", self.name)
 	if current_level == 2:
-		
-		$Timer.set_wait_time(randi()%10+1)
+		timeLeft = rand_range(1, 10)
 		if $Anim.animation == 'level_1':
 			$Anim.play("level_2")
 		elif $Anim.animation == 'level_2':
 			$Anim.play("level_1")
+
+func _process(delta):
+	if timeLeft > 0:
+		timeLeft -= delta
+		if timeLeft <= 0:
+			timeLeft = 0
+			lvl2Animation();
+			
